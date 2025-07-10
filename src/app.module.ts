332c85a1918e6729c27175a30b2065899f42576b
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-07-10 19:01:01 +0530

    add @nestjs/throttler package and configure throttling in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validateEnv } from '@app/common/config/validate-env';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validateEnv,
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
     }),
     AuthModule,
   ],
