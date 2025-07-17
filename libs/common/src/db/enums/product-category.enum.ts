5ff7f0edf345d20347b026450561015a55ef7366
jinia101 <jiniasingha10@gmail.com>
2025-07-17 13:57:17 +0530

    update product category enum

--- libs/common/src/db/enums/product-category.enum.ts
+++ libs/common/src/db/enums/product-category.enum.ts
@@ -5,7 +5,6 @@ export enum productCategory {
   FOOD_PRODUCTS = 'food_products',
   JEWELLERY = 'jewellery',
   STATIONERY = 'stationery',
-  ELECTRONICS = 'electronics',
   FURNITURE = 'furniture',
   TOYS = 'toys',
   OTHER = 'other',
